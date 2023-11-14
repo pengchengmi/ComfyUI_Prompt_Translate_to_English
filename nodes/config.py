0cd4da29c60b5c79cca0bf91001432c21086fc8f
shiertier <90256303+shiertier@users.noreply.github.com>
2023-11-15 02:21:43 +0800

    Update config.py

--- nodes/config.py
+++ nodes/config.py
@@ -5,11 +5,5 @@ ali_key_secret=""
 
 # 百度API
 ####################################
-<<<<<<< HEAD:nodes/config.py
 appid = ''      # 填写你的appid
 secretKey = ''  # 填写你的密钥
-=======
-appid = ''
-secretKey = ''
->>>>>>> 34f7d40c883b9d6ce0e0e384b84280d0b83fbe3d:config.py
-
