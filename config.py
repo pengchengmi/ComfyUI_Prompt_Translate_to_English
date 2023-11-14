34f7d40c883b9d6ce0e0e384b84280d0b83fbe3d
shiertier <90256303+shiertier@users.noreply.github.com>
2023-11-15 02:02:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
-# 使用阿里API需要修改的地方
+# 阿里API
 ####################################
 ali_key_id = ""
 ali_key_secret=""
 
-# BaiduTranslateApi version 使用API需要修改的地方
+# 百度API
 ####################################
-appid = ''      # 填写你的appid
-<<<<<<< HEAD
-secretKey = ''  # 填写你的密钥
-=======
-secretKey = ''  # 填写你的密钥
->>>>>>> b4e04b65e9202bb883d5dce3ea3708954eed2ab3
+appid = ''
+secretKey = ''
+
