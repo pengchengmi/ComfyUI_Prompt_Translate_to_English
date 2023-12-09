57fe143eb4d60819f640dc51664c7c22c2e22fcf
宓鹏程 <90431950+pengchengmi@users.noreply.github.com>
2023-12-09 15:18:07 +0800

    Update and rename pyexecjs.bat to 有问题就双击它.bat

--- pyexecjs.bat
+++
@@ -1,2 +0,0 @@
-..\..\..\python_embeded\python.exe -m pip install --upgrade pip
-..\..\..\python_embeded\python.exe -s -m pip install pyexecjs
---
+++ 有问题就双击它.bat
@@ -0,0 +1,5 @@
+..\..\..\python_embeded\python.exe -m pip install --upgrade pip
+..\..\..\python_embeded\python.exe -s -m pip install pyexecjs
+..\..\..\python_embeded\python.exe -s -m pip install requests
+..\..\..\python_embeded\python.exe -s -m pip install alibabacloud_alimt20181012
+..\..\..\python_embeded\python.exe -s -m pip install alibabacloud_tea_console
